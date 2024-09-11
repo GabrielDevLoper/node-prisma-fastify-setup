4befb064d4f88281877fbb9f0a36b446557cad60
Gabriel Barreto <gabriel.devloper@gmail.com>
2024-09-11 18:01:02 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+import fastify from "fastify";
+import { z } from "zod";
+
+
+const app = fastify();
+
+const prisma  = new PrismaClient();
+
+app.get("/users", async (req, reply) => {
+    const users = await prisma.user.findMany();
+
+    return users
+});
+
+app.post("/users", async (req, reply) => {
+    const createSchemaPayloadUser = z.object({
+        name: z.string(),
+        email: z.string().email()
+    })
+
+    const { name, email } = createSchemaPayloadUser.parse(req.body);
+
+    await prisma.user.create({
+         data: {
+            name, 
+            email,
+         }
+    });
+
+    return reply.status(201).send()
+});
+
+app.listen({
+    host: "0.0.0.0",
+    port: process.env.PORT ? Number(process.env.PORT) : 3333
+}).then(() => {
+    console.log("server is running");
+})
